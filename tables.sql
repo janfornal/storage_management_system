1576dae7283fa71f77b50b9fd18bd1966d303390
kuba.was <jakub93.was@student.uj.edu.pl>
2022-04-19 14:33:46 +0200

    translate

--- tables.sql
+++ tables.sql
@@ -1,103 +1,103 @@
 BEGIN;
-DROP TABLE IF EXISTS ZWROTY_KLIENTOW    CASCADE;
-DROP TABLE IF EXISTS PRODUKTY_SPRZEDAZ  CASCADE; --delete before PRODUKTY (COMMENT)
-DROP TABLE IF EXISTS SPRZEDAZE          CASCADE; --delete after PRODUKTY_SPRZEDAZ
-DROP TABLE IF EXISTS PRODUKTY_DOSTAWY   CASCADE;
-DROP TABLE IF EXISTS DOSTAWY            CASCADE;
-DROP TABLE IF EXISTS STAN_MAGAZYNU      CASCADE; --delete before PRODUKTY
-DROP TABLE IF EXISTS DOSTAWCY           CASCADE;
-DROP TABLE IF EXISTS HISTORIA_CEN       CASCADE; --delete before PRODUKTY
-DROP TABLE IF EXISTS PARAMETR_PRODUKTU  CASCADE;
-DROP TABLE IF EXISTS WYMAGANE_PARAMETRY CASCADE;
-DROP TABLE IF EXISTS PRODUKTY           CASCADE; --(COMMENT) 
-DROP TABLE IF EXISTS PARAMETRY          CASCADE;
-DROP TABLE IF EXISTS KATEGORIE          CASCADE;
-DROP TABLE IF EXISTS MARKA              CASCADE; --delete after PRODUKTY
+DROP TABLE IF EXISTS CLIENTS_RETURN      CASCADE;
+DROP TABLE IF EXISTS PRODUCTS_SOLD       CASCADE; --delete before PRODUKTY (COMMENT)
+DROP TABLE IF EXISTS SALES               CASCADE; --delete after PRODUKTY_SPRZEDAZ
+DROP TABLE IF EXISTS PRODUCTS_DELIVERIES CASCADE;
+DROP TABLE IF EXISTS DELIVERIES          CASCADE;
+DROP TABLE IF EXISTS STAN_MAGAZYNU       CASCADE; --delete before PRODUKTY
+DROP TABLE IF EXISTS SUPPLIERS           CASCADE;
+DROP TABLE IF EXISTS PRICE_HISTORY       CASCADE; --delete before PRODUKTY
+DROP TABLE IF EXISTS PARAMETER_PRODUCTS  CASCADE;
+DROP TABLE IF EXISTS REQUIRED_PARAMETERS CASCADE;
+DROP TABLE IF EXISTS PRODUCTS            CASCADE; --(COMMENT) 
+DROP TABLE IF EXISTS PARAMETERS          CASCADE;
+DROP TABLE IF EXISTS CATEGORIES          CASCADE;
+DROP TABLE IF EXISTS BRAND               CASCADE; --delete after PRODUKTY
 
 ---------------------------------------------------------------
 
-CREATE TABLE MARKA (
-    id NUMERIC(4) PRIMARY KEY,
-    nazwa VARCHAR(20)
+CREATE TABLE BRAND (
+    id_brand NUMERIC(4) PRIMARY KEY,
+    name VARCHAR(20)
 );
 
-CREATE TABLE KATEGORIE(
-    id NUMERIC(4) PRIMARY KEY,
-    nazwa varchar(40) NOT NULL UNIQUE,
+CREATE TABLE CATEGORIES(
+    id_category NUMERIC(4) PRIMARY KEY,
+    name VARCHAR(40) NOT NULL UNIQUE,
     vat NUMERIC(3) NOT NULL
 );
 
-CREATE TABLE PARAMETRY(
-    id NUMERIC(4) PRIMARY KEY,
-    nazwa varchar(100),
-    jednostka varchar(100)
+CREATE TABLE PARAMETERS(
+    id_parameter NUMERIC(4) PRIMARY KEY,
+    name VARCHAR(100),
+    unit VARCHAR(100)
 );
 
-CREATE TABLE PRODUKTY (
+CREATE TABLE PRODUCTS (
     id NUMERIC(10) PRIMARY KEY,
-    id_kategoria NUMERIC(6) REFERENCES KATEGORIE(id),
-    nazwa VARCHAR(20),
-    id_marki NUMERIC(4) REFERENCES MARKA(id)
+    id_category NUMERIC(6) REFERENCES CATEGORIES(id),
+    name VARCHAR(20),
+    id_brand NUMERIC(4) REFERENCES BRAND(id_brand)
 );
 
-CREATE TABLE WYMAGANE_PARAMETRY (
-    id_kategoria NUMERIC(4) NOT NULL REFERENCES KATEGORIE(id),
-    id_parametru NUMERIC(4) NOT NULL REFERENCES PARAMETRY(id)
+CREATE TABLE REQUIRED_PARAMETERS (
+    id_category NUMERIC(4) NOT NULL REFERENCES CATEGORIES(id_category),
+    id_parameter NUMERIC(4) NOT NULL REFERENCES PARAMETERS(id_parameter)
 );
 
-CREATE TABLE PARAMETR_PRODUKTY(
-    id_parametru NUMERIC(4) PRIMARY KEY,
-    id_produktu NUMERIC(4) NOT NULL REFERENCES PRODUKTY(id),
-    ilosc NUMERIC(4)
+CREATE TABLE PARAMETER_PRODUCTS(
+    id_parameter NUMERIC(4) NOT NULL,
+    id_product NUMERIC(4) NOT NULL REFERENCES PRODUCTS(id),
+    quantity NUMERIC(4)
 );
 
-CREATE TABLE HISTORIA_CEN (
-    id_produktu NUMERIC(10) REFERENCES PRODUKTY(id), --check if types agree
-    data_wprowadzenia DATE,
-    cena_netto NUMERIC(8,2)
+CREATE TABLE PRICE_HISTORY (
+    id_product NUMERIC(10) REFERENCES PRODUCTS(id), --check if types agree
+    launch_date DATE,
+    net_price NUMERIC(8,2)
 );
 
-CREATE TABLE DOSTAWCY (
-    id_dostawcy NUMERIC(10) PRIMARY KEY,
-    nazwa VARCHAR(20)
+CREATE TABLE SUPPLIERS (
+    id_supplier NUMERIC(10) PRIMARY KEY,
+    name VARCHAR(20)
 );
 
 CREATE TABLE STAN_MAGAZYNU (
-    id_produktu NUMERIC(10) REFERENCES PRODUKTY(id) UNIQUE, --check if types agree
-    ilosc NUMERIC(6) CHECK (ilosc >= 0)
+    id_product NUMERIC(10) REFERENCES PRODUCTS(id) UNIQUE, --check if types agree
+    quantity NUMERIC(6) CHECK (quantity >= 0)
 );
 
-CREATE TABLE DOSTAWY (
-    id_dostawy NUMERIC(10) PRIMARY KEY,
-    id_dostawcy NUMERIC(10) REFERENCES DOSTAWCY(id_dostawcy),
-    data_dostawy DATE
+CREATE TABLE DELIVERIES (
+    id_delivery NUMERIC(10) PRIMARY KEY,
+    id_supplier NUMERIC(10) REFERENCES DOSTAWCY(id_supplier),
+    date_delivery DATE
 );
 
-CREATE TABLE PRODUKTY_DOSTAWY (
-    id_dostawy NUMERIC(10) REFERENCES DOSTAWY(id_dostawy),
-    id_produktu NUMERIC(10) REFERENCES PRODUKTY(id),
-    CONSTRAINT pd_key UNIQUE (id_dostawy, id_produktu),
-    ilosc NUMERIC(6) CHECK (ilosc > 0)
+CREATE TABLE PRODUCTS_DELIVERIES (
+    id_delivery NUMERIC(10) REFERENCES DELIVERIES(id_delivery),
+    id_product NUMERIC(10) REFERENCES PRODUCTS(id),
+    CONSTRAINT pd_key UNIQUE (id_delivery, id_product),
+    quantity NUMERIC(6) CHECK (quantity > 0)
 );
 
-CREATE TABLE SPRZEDAZE (
-    id_sprzedazy NUMERIC(10) PRIMARY KEY,
-    data DATE
+CREATE TABLE SALES (
+    id_sale NUMERIC(10) PRIMARY KEY,
+    "date" DATE
 );
 
-CREATE TABLE PRODUKTY_SPRZEDAZ (
-    id_sprzedazy NUMERIC(10) REFERENCES SPRZEDAZE(id_sprzedazy),
-    id_produktu NUMERIC(10) REFERENCES PRODUKTY(id),
-    CONSTRAINT ps_key UNIQUE (id_sprzedazy, id_produktu),
-    ilosc NUMERIC(6) CHECK (ilosc > 0)
+CREATE TABLE PRODUCTS_SOLD (
+    id_sale NUMERIC(10) REFERENCES SALES(id_sale),
+    id_product NUMERIC(10) REFERENCES PRODUCTS(id),
+    CONSTRAINT ps_key UNIQUE (id_sale, id_product),
+    quantity NUMERIC(6) CHECK (quantity > 0)
 );
 
-CREATE TABLE ZWROTY_KLIENTOW (
-    id_zwrotu NUMERIC(10) PRIMARY KEY,
-    id_sprzedazy NUMERIC(10) REFERENCES SPRZEDAZE(id_sprzedazy),
-    id_produktu NUMERIC(10) REFERENCES PRODUKTY(id),
-    ilosc NUMERIC(6) CHECK (ilosc > 0),
-    data DATE
+CREATE TABLE CLIENTS_RETURN (
+    id_return NUMERIC(10) PRIMARY KEY,
+    id_sale NUMERIC(10) REFERENCES SALES(id_sale),
+    id_product NUMERIC(10) REFERENCES PRODUCTS(id),
+    quantity NUMERIC(6) CHECK (quantity > 0),
+    "date" DATE
 );
 
 ---------------------------------------------------------------
@@ -107,28 +107,28 @@ CREATE OR REPLACE FUNCTION zwroty_ilosc_check(NUMERIC, NUMERIC)
 $$
 BEGIN
     RETURN (
-        SELECT COALESCE(SUM(ilosc), 0)
-        FROM PRODUKTY_SPRZEDAZ
-        WHERE id_sprzedazy = $1 AND id_produktu = $2
+        SELECT COALESCE(SUM(quantity), 0)
+        FROM PRODUCTS_SOLD
+        WHERE id_sale = $1 AND id_product = $2
     )-(
-        SELECT COALESCE(SUM(ilosc), 0)
+        SELECT COALESCE(SUM(quantity), 0)
         FROM ZWROTY_KLIENTOW
-        WHERE id_sprzedazy = $1 AND id_produktu = $2
+        WHERE id_sale = $1 AND id_product = $2
     );
 END;
 $$ LANGUAGE 'plpgsql';
 
 --additional constraint to control zwroty ilosc
 ALTER TABLE ZWROTY_KLIENTOW
-ADD CONSTRAINT zwroty_check CHECK(zwroty_ilosc_check(id_sprzedazy, id_produktu) >= 0);
+ADD CONSTRAINT zwroty_check CHECK(zwroty_ilosc_check(id_sale, id_product) >= 0);
 
 CREATE OR REPLACE FUNCTION update_decrease_stan_magazynu()
     RETURNS trigger AS
 $$
 BEGIN
     UPDATE STAN_MAGAZYNU
-    SET ilosc = ilosc - NEW.ilosc
-    WHERE id_produktu = NEW.id_produktu;
+    SET quantity = quantity - NEW.quantity
+    WHERE id_product = NEW.id_product;
     RETURN NEW;
 END;
 $$ LANGUAGE 'plpgsql';
@@ -138,8 +138,8 @@ CREATE OR REPLACE FUNCTION update_increase_stan_magazynu()
 $$
 BEGIN
     UPDATE STAN_MAGAZYNU
-    SET ilosc = ilosc + NEW.ilosc
-    WHERE id_produktu = NEW.id_produktu;
+    SET quantity = quantity + NEW.quantity
+    WHERE id_product = NEW.id_product;
     RETURN NEW;
 END;
 $$ LANGUAGE 'plpgsql';
@@ -155,7 +155,7 @@ EXECUTE PROCEDURE update_decrease_stan_magazynu();
 --updates record in stan_magazynu, after inserting into PRODUKTY_DOSTAWY
 CREATE TRIGGER trigger_ilosc_pd  
 AFTER INSERT
-ON PRODUKTY_DOSTAWY
+ON PRODUCTS_DELIVERIES
 FOR EACH ROW
 EXECUTE PROCEDURE update_increase_stan_magazynu();
 
@@ -163,7 +163,7 @@ CREATE OR REPLACE FUNCTION insert_id_products_to_stan_magazynu()
     RETURNS trigger AS
 $$
 BEGIN
-    INSERT INTO STAN_MAGAZYNU (id_produktu, ilosc) VALUES (NEW.id, 0);
+    INSERT INTO STAN_MAGAZYNU (id_produktu, quantity) VALUES (NEW.id, 0);
     RETURN NEW;
 END;
 $$ LANGUAGE 'plpgsql';
@@ -171,7 +171,7 @@ $$ LANGUAGE 'plpgsql';
 --name of trigger is long, but self-explanatory
 CREATE TRIGGER trigger_add_id_product_to_stan_magazynu  
 AFTER INSERT
-ON PRODUKTY
+ON PRODUCTS
 FOR EACH ROW
 EXECUTE PROCEDURE insert_id_products_to_stan_magazynu();
 
