430d34adedfc572bc51b9572e84d6ca787c97e5e
Kacper Topolski <kacpertopolski@op.pl>
2022-06-04 19:52:29 +0200

    Small changes to some triggers

--- clear.sql
+++ clear.sql
@@ -24,9 +24,9 @@ DROP TABLE IF EXISTS BRAND               CASCADE;
 
 
 DROP FUNCTION IF EXISTS returned_check()                        CASCADE;
-DROP FUNCTION IF EXISTS allowed_param_check()                   CASCADE;  
-DROP FUNCTION IF EXISTS update_decrease_store_status()          CASCADE;
-DROP FUNCTION IF EXISTS update_increase_store_status()          CASCADE;
+DROP FUNCTION IF EXISTS allowed_param_check()                   CASCADE;
+DROP FUNCTION IF EXISTS check_insert_products_sold()            CASCADE;
+DROP FUNCTION IF EXISTS check_delete_products_sold()            CASCADE;
 DROP FUNCTION IF EXISTS update_increase_store_status_2()        CASCADE;
 DROP FUNCTION IF EXISTS insert_id_products_to_store_status()    CASCADE;
 
